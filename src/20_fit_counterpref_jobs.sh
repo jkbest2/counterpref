553c0235b58bdca2b52c9e07b47f047f15469db0
John Best <jkbest@gmail.com>
2021-03-07 15:19:39 -0800

    Add fitting scripts

---
+++ src/20_fit_counterpref_jobs.sh
@@ -0,0 +1,100 @@
+Rscript src/20_fit_counterpref_parallel.R   1   1
+Rscript src/20_fit_counterpref_parallel.R   2   2
+Rscript src/20_fit_counterpref_parallel.R   3   3
+Rscript src/20_fit_counterpref_parallel.R   4   4
+Rscript src/20_fit_counterpref_parallel.R   5   5
+Rscript src/20_fit_counterpref_parallel.R   6   6
+Rscript src/20_fit_counterpref_parallel.R   7   7
+Rscript src/20_fit_counterpref_parallel.R   8   8
+Rscript src/20_fit_counterpref_parallel.R   9   9
+Rscript src/20_fit_counterpref_parallel.R  10  10
+Rscript src/20_fit_counterpref_parallel.R  11  11
+Rscript src/20_fit_counterpref_parallel.R  12  12
+Rscript src/20_fit_counterpref_parallel.R  13  13
+Rscript src/20_fit_counterpref_parallel.R  14  14
+Rscript src/20_fit_counterpref_parallel.R  15  15
+Rscript src/20_fit_counterpref_parallel.R  16  16
+Rscript src/20_fit_counterpref_parallel.R  17  17
+Rscript src/20_fit_counterpref_parallel.R  18  18
+Rscript src/20_fit_counterpref_parallel.R  19  19
+Rscript src/20_fit_counterpref_parallel.R  20  20
+Rscript src/20_fit_counterpref_parallel.R  21  21
+Rscript src/20_fit_counterpref_parallel.R  22  22
+Rscript src/20_fit_counterpref_parallel.R  23  23
+Rscript src/20_fit_counterpref_parallel.R  24  24
+Rscript src/20_fit_counterpref_parallel.R  25  25
+# Rscript src/20_fit_counterpref_parallel.R  26  26
+# Rscript src/20_fit_counterpref_parallel.R  27  27
+# Rscript src/20_fit_counterpref_parallel.R  28  28
+# Rscript src/20_fit_counterpref_parallel.R  29  29
+# Rscript src/20_fit_counterpref_parallel.R  30  30
+# Rscript src/20_fit_counterpref_parallel.R  31  31
+# Rscript src/20_fit_counterpref_parallel.R  32  32
+# Rscript src/20_fit_counterpref_parallel.R  33  33
+# Rscript src/20_fit_counterpref_parallel.R  34  34
+# Rscript src/20_fit_counterpref_parallel.R  35  35
+# Rscript src/20_fit_counterpref_parallel.R  36  36
+# Rscript src/20_fit_counterpref_parallel.R  37  37
+# Rscript src/20_fit_counterpref_parallel.R  38  38
+# Rscript src/20_fit_counterpref_parallel.R  39  39
+# Rscript src/20_fit_counterpref_parallel.R  40  40
+# Rscript src/20_fit_counterpref_parallel.R  41  41
+# Rscript src/20_fit_counterpref_parallel.R  42  42
+# Rscript src/20_fit_counterpref_parallel.R  43  43
+# Rscript src/20_fit_counterpref_parallel.R  44  44
+# Rscript src/20_fit_counterpref_parallel.R  45  45
+# Rscript src/20_fit_counterpref_parallel.R  46  46
+# Rscript src/20_fit_counterpref_parallel.R  47  47
+# Rscript src/20_fit_counterpref_parallel.R  48  48
+# Rscript src/20_fit_counterpref_parallel.R  49  49
+# Rscript src/20_fit_counterpref_parallel.R  50  50
+# Rscript src/20_fit_counterpref_parallel.R  51  51
+# Rscript src/20_fit_counterpref_parallel.R  52  52
+# Rscript src/20_fit_counterpref_parallel.R  53  53
+# Rscript src/20_fit_counterpref_parallel.R  54  54
+# Rscript src/20_fit_counterpref_parallel.R  55  55
+# Rscript src/20_fit_counterpref_parallel.R  56  56
+# Rscript src/20_fit_counterpref_parallel.R  57  57
+# Rscript src/20_fit_counterpref_parallel.R  58  58
+# Rscript src/20_fit_counterpref_parallel.R  59  59
+# Rscript src/20_fit_counterpref_parallel.R  60  60
+# Rscript src/20_fit_counterpref_parallel.R  61  61
+# Rscript src/20_fit_counterpref_parallel.R  62  62
+# Rscript src/20_fit_counterpref_parallel.R  63  63
+# Rscript src/20_fit_counterpref_parallel.R  64  64
+# Rscript src/20_fit_counterpref_parallel.R  65  65
+# Rscript src/20_fit_counterpref_parallel.R  66  66
+# Rscript src/20_fit_counterpref_parallel.R  67  67
+# Rscript src/20_fit_counterpref_parallel.R  68  68
+# Rscript src/20_fit_counterpref_parallel.R  69  69
+# Rscript src/20_fit_counterpref_parallel.R  70  70
+# Rscript src/20_fit_counterpref_parallel.R  71  71
+# Rscript src/20_fit_counterpref_parallel.R  72  72
+# Rscript src/20_fit_counterpref_parallel.R  73  73
+# Rscript src/20_fit_counterpref_parallel.R  74  74
+# Rscript src/20_fit_counterpref_parallel.R  75  75
+# Rscript src/20_fit_counterpref_parallel.R  76  76
+# Rscript src/20_fit_counterpref_parallel.R  77  77
+# Rscript src/20_fit_counterpref_parallel.R  78  78
+# Rscript src/20_fit_counterpref_parallel.R  79  79
+# Rscript src/20_fit_counterpref_parallel.R  80  80
+# Rscript src/20_fit_counterpref_parallel.R  81  81
+# Rscript src/20_fit_counterpref_parallel.R  82  82
+# Rscript src/20_fit_counterpref_parallel.R  83  83
+# Rscript src/20_fit_counterpref_parallel.R  84  84
+# Rscript src/20_fit_counterpref_parallel.R  85  85
+# Rscript src/20_fit_counterpref_parallel.R  86  86
+# Rscript src/20_fit_counterpref_parallel.R  87  87
+# Rscript src/20_fit_counterpref_parallel.R  88  88
+# Rscript src/20_fit_counterpref_parallel.R  89  89
+# Rscript src/20_fit_counterpref_parallel.R  90  90
+# Rscript src/20_fit_counterpref_parallel.R  91  91
+# Rscript src/20_fit_counterpref_parallel.R  92  92
+# Rscript src/20_fit_counterpref_parallel.R  93  93
+# Rscript src/20_fit_counterpref_parallel.R  94  94
+# Rscript src/20_fit_counterpref_parallel.R  95  95
+# Rscript src/20_fit_counterpref_parallel.R  96  96
+# Rscript src/20_fit_counterpref_parallel.R  97  97
+# Rscript src/20_fit_counterpref_parallel.R  98  98
+# Rscript src/20_fit_counterpref_parallel.R  99  99
+# Rscript src/20_fit_counterpref_parallel.R 100 100
---
+++ src/20_fit_counterpref_parallel.R
@@ -0,0 +1,5 @@
+library(RhpcBLASctl)
+
+blas_set_num_threads(4)
+
+source("src/20_fit_counterpref.R")
